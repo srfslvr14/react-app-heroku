d77f86ddfe002027f9274854f2f1c9b417a30e53
Spencer Fulgham <spencerman@comcast.net>
2021-08-02 17:15:02 -0700

    change

--- react-app-heroku/src/App.js
+++ react-app-heroku/src/App.js
@@ -9,6 +9,7 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
+        <h1> Kaitlyn has a tier 1 ass </h1>
         <a
           className="App-link"
           href="https://reactjs.org"
