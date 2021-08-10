07199c0ff31db1b0e4db4cbec5063269350dc82e
Spencer Fulgham <spencerman@comcast.net>
2021-08-09 20:48:34 -0700

    comp

--- src/App.js
+++ src/App.js
@@ -1,13 +1,11 @@
 import logo from './logo.svg';
 import './App.css';
+import Layout from './Comp/Layout.js' 
 
 function App() {
   return (
     <div>
-      <h1> Welcome to my personal website, here is what I have to say: </h1>
-      <h1> I want to leave hickeys all up and down Kaitlyn's body and make her squerm </h1>
-      <h2> as well as grab her waist and pull her on top of me</h2>
-      <p> but also cuddle and be warm and hold her tight and kiss her cute cheeks</p>
+      <Layout/>
     </div>
   );
 }
---
+++ src/Comp/Layout.js
@@ -0,0 +1,15 @@
+import Body from "./Body";
+import Header from "./Header";
+import Footer from "./Footer";
+
+function Layout() {
+  return (
+    <div>
+      <Header/>
+      <Body/>
+      <Footer/>
+    </div>
+  );
+}
+
+export default Layout;
