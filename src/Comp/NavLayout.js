07b2290261494658f8ca1fb0c278f5528721adec
Spencer Fulgham <spencerman@comcast.net>
2022-03-22 01:16:42 -0700

    navbar and cards

--- src/Comp/NavLayout.js
+++ src/Comp/NavLayout.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import Controller from './Controller.js';
-import {Navbar, Icon} from "materialize-css"
 
 import {
     BrowserRouter as Router, Link
@@ -12,45 +11,23 @@ class NavLayout extends React.Component{
 
   render(){
     return(
-        <div>
-
-
-            <Router>
-                <div>
-                    <nav class="navbar navbar-dark bg-dark navbar-expand-lg">
-                        <a class="navbar-brand" href="#"> SRayFulgham </a>
-                        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-                            <span class="navbar-toggler-icon"></span>
-                        </button>
-
-                        <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                            <ul class="navbar-nav mr-auto">
-
-                                <li class="nav-item active">
-                                    <a class="nav-link"> <Link to="/">Home</Link></a>
-                                </li>
-
-                                <li class="nav-item">
-                                    <a class="nav-link"> <Link to="/about">About</Link></a>
-                                </li>
-                                <li class="nav-item">
-                                    <a class="nav-link"> <Link to="/projects">Projects</Link></a>
-                                </li>
-                                <li class="nav-item">
-                                    <a class="nav-link"> <Link to="/resume">Resume</Link></a>
-                                </li>
-                                <li class="nav-item">
-                                    <a class="nav-link"> <Link to="/contact">Contact</Link></a>
-                                </li>
-
-                            
-                            </ul>
-                        </div>
-                    </nav>
-                    <Controller/>
+        <Router>
+            <nav>
+                <div class="nav-wrapper">
+                <a href="/" class="brand-logo">SRF</a>
+                <ul id="nav-mobile" class="right hide-on-med-and-down">
+                    <li><a href="/about">   About   </a></li>
+                    <li><a href="/projects">Projects</a></li>
+                    <li><a href="/resume">  Resume  </a></li>
+                    <li><a href="/Contact"> Contact </a></li>
+                    {/* <li><a href="/about">   About   </a></li> */}
+                    {/* <li><a href="badges.html">Components</a></li>
+                    <li class="active"><a href="collapsible.html">JavaScript</a></li> */}
+                </ul>
                 </div>
-            </Router>
-        </div>
+            </nav>
+            <Controller/>
+        </Router>
     );
   }
 } 
--- src/Comp/ProjectPage.js
+++ src/Comp/ProjectPage.js
@@ -1,9 +1,55 @@
 import React from "react";
 
 export default function ProjectPage() {
+
+
   return (
     <div>
-      project
+
+    <div class="row">
+        <div class="col s12 m5">
+          <div class="card hoverable medium rounded" Style="width:400px;">
+            <div class="card-image waves-effect waves-block waves-light">
+              <img src="https://cdn.mos.cms.futurecdn.net/ntFmJUZ8tw3ULD3tkBaAtf.jpg"/>
+              <span class="card-title">Card Title</span>
+            </div>
+            <div class="card-content">
+              <p>I am a very simple card. I am good at containing small bits of information.
+              I am convenient because I require little markup to use effectively.</p>
+            </div>
+            <div class="card-action">
+              <a href="#">This is a link</a>
+            </div>
+          </div>
+
+          <div class="card hoverable medium" Style="width:400px;">
+            <div class="card-image waves-effect waves-block waves-light">
+              <img class="activator" src="https://cdn.mos.cms.futurecdn.net/ntFmJUZ8tw3ULD3tkBaAtf.jpg"/>
+            </div>
+            <div class="card-content">
+              <span class="card-title activator grey-text text-darken-4">Card Title<i class="material-icons right">more_vert</i></span>
+              <p><a href="#">This is a link</a></p>
+            </div>
+            <div class="card-reveal">
+              <span class="card-title grey-text text-darken-4"><i class="material-icons right">close</i>Card Title</span>
+              <p>Here is some more information about this product that is only revealed once clicked on.</p>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <a class="waves-effect waves-light btn modal-trigger" href="#modal1">Modal</a>
+      
+      <div id="modal1" class="modal">
+        <div class="modal-content">
+          <h4>Modal Header</h4>
+          <p>A bunch of text</p>
+        </div>
+        <div class="modal-footer">
+          <a href="#!" class="modal-action modal-close waves-effect waves-green btn-flat">Agree</a>
+        </div>
+      </div>
+
     </div>
   );
 }
