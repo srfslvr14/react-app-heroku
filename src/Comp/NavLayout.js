28767040ddddb3117a03e887162b2e947032516d
Spencer Fulgham <spencerman@comcast.net>
2022-03-22 20:27:08 -0700

    mobile sex

--- src/Comp/NavLayout.js
+++ src/Comp/NavLayout.js
@@ -30,7 +30,7 @@ class NavLayout extends React.Component{
                             <img src={home} alt="home" Style="width:18% "/>
                             SRF
                         </a>
-                        <ul id="nav-mobile" class="right hide-on-small">
+                        <ul id="nav-mobile" class="right hide-on-small-only">
                             <li><a href="/about">   About   </a></li>
                             <li><a href="/projects">Projects</a></li>
                             <li><a href={ResumePage}> Resume  </a></li>
