d6a77af56b3258bc83781de7593a68af782c79d1
Spencer Fulgham <spencerman@comcast.net>
2022-03-24 00:19:00 -0700

    fonttt

--- src/Comp/MyExper.js
+++ src/Comp/MyExper.js
@@ -1,10 +1,9 @@
-import React from "react";
-// import TLcss from '../vertical-timeline-2/css/style.css'
+import TLcss from '../vertical-timeline-2/css/style.css' 
 
 export default function MyExper() {
   return ( 
     <div>
-      <section class="timeline" Style="margin-top:-5% ../vertical-timeline-2/css/style.css">
+      <section class="timeline" Style="margin-top:-5%">
         <div class="containerTL">
           <div class="timeline-item">
             <div class="timeline-img teal accent-4"></div>
