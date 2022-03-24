6af42e154c60a9875be8ed30a0a2459b1c0e4f79
Spencer Fulgham <spencerman@comcast.net>
2022-03-24 03:02:11 -0700

    exp rd 1

--- src/Comp/MyExper.js
+++ src/Comp/MyExper.js
@@ -15,12 +15,28 @@ export default function MyExper() {
             </div>
               <div class="date indigo lighten-1">Today</div>
                 <p>
-                  In a project  sponsored by the Joseph M. Long Marine Laboratory at UC Santa Cruz,
+                  These days, in a project sponsored by the Joseph M. Long Marine Laboratory at UC Santa Cruz,
                   Spencer works as part of a small team of software developers designing and producing a web 
-                  application to visualize marine debris data collected by LML. <p/>
+                  application to visualize marine debris data collected by LML.
                 </p>
+                <br/>
             </div>
           </div> 
+
+          {/* Cruzhacks 22 */}
+          <div class="timeline-item">
+            <div class="timeline-img teal accent-4"></div>
+            <div class="timeline-content js--fadeInLeft">
+              <div class="date indigo lighten-1">JAN 16, 2022</div>
+              <h2 Style="margin-top:2%">Cruzhacks 2022!</h2>
+  
+              <blockquote>
+                Spencer participated in Cruzhacks, 
+                developing a Google Chrome Extension called "Check-in Chicken," 
+                a mental health focused Pomodoro study assisent.
+              </blockquote>
+            </div>
+          </div>  
           
           {/* CSE 160 */}
           <div class="timeline-item">
@@ -36,97 +52,143 @@ export default function MyExper() {
                 understand course materials, how to apply course topics to the coding assignnments, 
                 and general debugging skills.
                 </p>
+                <br/>
             </div>
-
-          </div>   
+          </div>      
           
+          {/* Coding Minds */}
           <div class="timeline-item">
-
             <div class="timeline-img teal accent-4"></div>
-
-            <div class="timeline-content js--fadeInLeft">
-              <div class="date">3 JUN 2016</div>
-              <h2>Quote</h2>
-              <blockquote>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dicta explicabo debitis omnis dolor iste fugit totam quasi inventore!</blockquote>
-            </div>
-          </div>   
-          
-          <div class="timeline-item">
-
-            <div class="timeline-img teal accent-4"></div>
-
-            <div class="timeline-content js--fadeInRight">
-              <h2>Title</h2>
-              <div class="date">22 JUN 2016</div>
-              <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Maxime ipsa ratione omnis alias cupiditate saepe atque totam aperiam sed nulla voluptatem recusandae dolor, nostrum excepturi amet in dolores. Alias, ullam.</p>
-              <a class="bnt-more" href="javascript:void(0)">More</a>
-            </div>
-          </div>   
-          
-          <div class="timeline-item">
-
-            <div class="timeline-img teal accent-4"></div>
-
-            <div class="timeline-content timeline-card js--fadeInLeft">
-              <div class="timeline-img-header">
-                <h2>Card Title</h2>
+            <div class="timeline-content timeline-card js--fadeInRight">
+              <div class="timeline-img-header" Style="height:100px">
+                <h2>Coding Minds Online Instructor</h2>
               </div>
-              <div class="date">10 JULY 2016</div>
-              <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Maxime ipsa ratione omnis alias cupiditate saepe atque totam aperiam sed nulla voluptatem recusandae dolor, nostrum excepturi amet in dolores. Alias, ullam.</p>
-              <a class="bnt-more" href="javascript:void(0)">More</a>
+                <div class="date indigo lighten-1">JUN 2021 - OCT 2021</div>
+                <p>
+                  Spencer virtually led small groups of children to further the 
+                  students' understanding and confidence in coding with Python and Lua.
+                  Seeing the students grow and learn over the 13-week course made Spencer's
+                  immensely proud of the students and the work they acomplished.
+                </p>
+                <br/>
             </div>
           </div>   
 
-          <div class="timeline-item">
-
+         {/* ID Tech */}
+         <div class="timeline-item">
             <div class="timeline-img teal accent-4"></div>
+            <div class="timeline-content timeline-card js--fadeInRight">
+              <div class="timeline-img-header" Style="height:100px">
+                <h2>iD Tech Remote Instructor</h2>
+              </div>
+                <div class="date indigo lighten-1">MAY 2021 - SEPT 2021</div>
+                <p>
+                  Over the summer of 2021, Spencer remotely taught small classes of young students 
+                  (aged 5-13) the fundimentals of Minecraft Modding in javascript. Spencer loved to
+                  see the creative items and mods students would develop over the course of the week.
+                </p>
+                <br/>
+            </div>
+          </div> 
 
+          {/* Groupify */}
+          <div class="timeline-item">
+            <div class="timeline-img teal accent-4"></div>
+            <div class="timeline-content js--fadeInLeft">
+              <div class="date indigo lighten-1">MAR 2021 - JUN 2021</div>
+              <h2 Style="margin-top:2%">Groupify Web Application</h2>
+              <blockquote>
+                His first exposure to a software development environment, Spencer 
+                worked with a team to design and produce a social website to improve on Spotify's
+                music stream webplayer. The team designed custom user profiles, a 
+                reccomendation system, and a group listen feature, all while using professional Scrum practices. 
+                Groupify is currently deployed and hosted online for any Spotify user to enjoy!
+              </blockquote>
+            </div>
+          </div>  
+
+          {/* Pawfect Match */}
+          <div class="timeline-item">
+            <div class="timeline-img teal accent-4"></div>
+            <div class="timeline-content js--fadeInLeft">
+              <div class="date indigo lighten-1">MAR 2021 - MAY 2021</div>
+              <h2 Style="margin-top:2%">Pawfect Match</h2>
+              <blockquote>
+                As a school assignment, Spencer and a small team of programmers 
+                developed a website to help users easily and intuitivly find local pets to adopt.
+                The website mimiced a swipeable dating app, and woulld directly connect users to local dog shelters.
+              </blockquote>
+            </div>
+          </div> 
+          
+          {/* Cruzhacks 21 */}
+          <div class="timeline-item">
+            <div class="timeline-img teal accent-4"></div>
+            <div class="timeline-content js--fadeInLeft">
+              <div class="date indigo lighten-1">JAN 17, 2021</div>
+              <h2 Style="margin-top:2%">Cruzhacks 2021!</h2>
+              <blockquote>
+                Spencer participated in Cruzhacks, 
+                developing a Discord Chat Bot cheekily named called "Bigby's Bot of Everything.
+                Bigby's was developed to help referencing Dungeons and Dragons rules and content 
+                while playing with friends online during quarentine.
+              </blockquote>
+            </div>
+          </div>   
+
+          {/* CSE 150 */}
+          <div class="timeline-item">
+            <div class="timeline-img teal accent-4"></div>
             <div class="timeline-content timeline-card js--fadeInRight">
               <div class="timeline-img-header">
-                <h2>Card Title</h2>
+                <h2>College Tutor and Grader: Intro to Computer Networks</h2>
               </div>
-              <div class="date">30 JULY 2016</div>
-              <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Maxime ipsa ratione omnis alias cupiditate saepe atque totam aperiam sed nulla voluptatem recusandae dolor, nostrum excepturi amet in dolores. Alias, ullam.</p>
-              <a class="bnt-more" href="javascript:void(0)">More</a>
+                <div class="date indigo lighten-1">JAN 2021 - JUN 2021</div>
+                <p>
+                  For the first half of 2021, Spencer worked with UC Santa Cruz's Introduction to 
+                  Computer Networks course as a tutor and grader. The course focused around teaching 
+                  students the fundimentals of the internet and networks.
+                  As a tutor, Spencer helped students understand course topics and how to apply these topics to the coding assignnments.
+                </p>
+                <br/>
             </div>
           </div>  
-          
+
+          {/* Crown Student Senate */}
           <div class="timeline-item">
-
             <div class="timeline-img teal accent-4"></div>
-
-            <div class="timeline-content js--fadeInLeft">
-              <div class="date">5 AUG 2016</div>
-              <h2>Quote</h2>
-              <blockquote>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dicta explicabo debitis omnis dolor iste fugit totam quasi inventore!</blockquote>
-            </div>
-          </div>   
-
-          <div class="timeline-item">
-
-            <div class="timeline-img teal accent-4"></div>
-
             <div class="timeline-content timeline-card js--fadeInRight">
-              <div class="timeline-img-header">
-                <h2>Card Title</h2>
+              <div class="timeline-img-header" Style="height:90px">
+                <h2>Crown Student Senator</h2>
               </div>
-              <div class="date">19 AUG 2016</div>
-              <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Maxime ipsa ratione omnis alias cupiditate saepe atque totam aperiam sed nulla voluptatem recusandae dolor, nostrum excepturi amet in dolores. Alias, ullam.</p>
-              <a class="bnt-more" href="javascript:void(0)">More</a>
+                <div class="date indigo lighten-1">OCT 2018 - JUN 2021</div>
+                <p>
+                  For his first 3 years of college, Spencer held a position of senator 
+                  for his college's student government. He helped to plan and run school events,
+                  approved funding for clubs requesting aid, and attending weekly senate meetings.
+                </p>
+                <br/>
             </div>
           </div>  
-          
+
+          {/* ASB */}
           <div class="timeline-item">
-
             <div class="timeline-img teal accent-4"></div>
-
-            <div class="timeline-content js--fadeInLeft">
-              <div class="date">1 SEP 2016</div>
-              <h2>Title</h2>
-              <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Maxime ipsa ratione omnis alias cupiditate saepe atque totam aperiam sed nulla voluptatem recusandae dolor, nostrum excepturi amet in dolores. Alias, ullam.</p>
-              <a class="bnt-more" href="javascript:void(0)">More</a>
+            <div class="timeline-content timeline-card js--fadeInRight">
+              <div class="timeline-img-header" Style="height:100px">
+                <h2>ASB Commissioner of Activities</h2>
+              </div>
+                <div class="date indigo lighten-1">JUL 2017 - JUN 2018</div>
+                <p>
+                  Spencer's first true work experience was when he held the position of Commissioner of Activities 
+                  in his high school's ASB student government. Spencer was the head of all school dances and pep rallies on campus,
+                  managing a class of about 50 students and a dozen commities per school activity. This role 
+                  taught Spencer many social, professional, and work skills, and he holds his time in ASB very dear to his heart.
+                </p>
+                <br/>
             </div>
           </div>  
+
         </div>
       </section>
     </div>
