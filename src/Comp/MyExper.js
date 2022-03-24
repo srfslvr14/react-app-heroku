9c95e17360345c5fac9d1c62e720ec9c2f1ece07
Spencer Fulgham <spencerman@comcast.net>
2022-03-24 01:23:05 -0700

    first few exp

--- src/Comp/MyExper.js
+++ src/Comp/MyExper.js
@@ -5,28 +5,37 @@ export default function MyExper() {
     <div>
       <section class="timeline" Style="margin-top:-5%">
         <div class="containerTL">
-          <div class="timeline-item">
+          
+          {/* LML */}
+          <div class="timeline-item" >
             <div class="timeline-img teal accent-4"></div>
-
-            <div class="timeline-content js--fadeInLeft">
-              <h2>Title</h2>
-              <div class="date">1 MAY 2016</div>
-              <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Maxime ipsa ratione omnis alias cupiditate saepe atque totam aperiam sed nulla voluptatem recusandae dolor, nostrum excepturi amet in dolores. Alias, ullam.</p>
-              <a class="bnt-more" href="javascript:void(0)">More</a>
+            <div class="timeline-content timeline-card js--fadeInLeft">
+            <div class="timeline-img-header"> 
+              <h2 Style="margin-right:10%"> Marine Debris Data Visualization Tool Developer</h2>
+            </div>
+              <div class="date">Today</div>
+                <p>
+                  In a project  sponsored by the Joseph M. Long Marine Laboratory at UC Santa Cruz,
+                  Spencer works as part of a small team of software developers designing and producing a web 
+                  application to visualize marine debris data collected by LML.
+                </p>
             </div>
           </div> 
           
+          {/* CSE 160 */}
           <div class="timeline-item">
-
             <div class="timeline-img teal accent-4"></div>
-
             <div class="timeline-content timeline-card js--fadeInRight">
               <div class="timeline-img-header">
-                <h2>Card Title</h2>
+                <h2>College Tutor and Grader: Intro to Computer Graphics</h2>
               </div>
-              <div class="date">25 MAY 2016</div>
-              <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Maxime ipsa ratione omnis alias cupiditate saepe atque totam aperiam sed nulla voluptatem recusandae dolor, nostrum excepturi amet in dolores. Alias, ullam.</p>
-              <a class="bnt-more" href="javascript:void(0)">More</a>
+                <div class="date">SEPT 2021 - MAR 2022</div>
+                <p>
+                For the ladder half of 2021, Spencer worked with UC Santa Cruz's Introduction to 
+                Computer Graphics course as a tutor and grader. As a tutor, Spencer helped students 
+                understand course materials, how to apply course topics to the coding assignnments, 
+                and general debugging skills.
+                </p>
             </div>
 
           </div>   
