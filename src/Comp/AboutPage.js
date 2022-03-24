3b574b322e9ae6af0eae9b54cf031ee7d0ae333d
Spencer Fulgham <spencerman@comcast.net>
2022-03-24 00:11:59 -0700

    dfont?

--- src/Comp/AboutPage.js
+++ src/Comp/AboutPage.js
@@ -3,28 +3,6 @@ import React from "react";
 import MyStory from './aboutSites/MyStory.js'
 
 export default class AboutPage extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      StoryPage: true,
-    };
-    this.gotoStory = this.gotoStory.bind(this);
-    this.gotoExper = this.gotoExper.bind(this);
-  }
-
-
-  gotoStory(){
-    this.setState({
-      StoryPage: true,
-    });
-  }
-
-  gotoExper(){
-    this.setState({
-      StoryPage: false,
-    });
-  }
-
   render(){
     return ( 
       <div>
